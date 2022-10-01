06939f4428b3dd73dc615a82e7bd6430defe2c6a
nyteghost <nytemmo@gmail.com>
2022-09-30 20:04:54 -0400

    Update teamMessage.py

--- MS-Teams/teamMessage.py
+++ MS-Teams/teamMessage.py
@@ -2,16 +2,16 @@ import requests
 import json
 
 
-url = "https://scaatlcloud.webhook.office.com/webhookb2/" \
+url = "https://<company>.webhook.office.com/webhookb2/" \
         "089d231b-35ef-417e-9400-9aba2e4016e2@60924edf-0a5e-4596-99cb-ebcf9d2ddb51/" \
         "IncomingWebhook/9086e2528b6a4ef7862f0f7e7eab008d/" \
         "542cb2d2-c180-43ed-bd24-3e2cf905ac74"
 
 
 payload = {
-    "text": "@nbowman@scacloud.com hello"
-            "@mbrown@scacloud.com hello"
-            "@aadeli@scacloud.com hello"
+    "text": "@email hello"
+            "@email hello"
+            "@email hello"
 }
 headers = {
     'Content-Type': 'application/json'
